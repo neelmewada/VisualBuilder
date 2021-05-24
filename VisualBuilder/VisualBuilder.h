2f6f3e71f8916d1d71c2bf174144fe9493908d2f
Neel Mewada <neelrmewada@gmail.com>
2021-05-24 09:51:24 +0530

    Initial Commit

---
+++ VisualBuilder/VisualBuilder.h
@@ -0,0 +1,18 @@
+//
+//  VisualBuilder.h
+//  VisualBuilder
+//
+//  Created by Neel Mewada on 24/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisualBuilder.
+FOUNDATION_EXPORT double VisualBuilderVersionNumber;
+
+//! Project version string for VisualBuilder.
+FOUNDATION_EXPORT const unsigned char VisualBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisualBuilder/PublicHeader.h>
+
+
